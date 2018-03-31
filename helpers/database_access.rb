88ce6ca63601d3ba13c47e6fad2db75244608883
Stephen Karger <stephen.karger@gmail.com>
2018-03-31 11:55:41 -0400

    Set up DB constant in Startup

--- helpers/database_access.rb
+++ helpers/database_access.rb
@@ -3,7 +3,7 @@ require 'sequel'
 module DatabaseAccess
   # this must be defined before DB constant set
   module_function def connect_options
-    ENV['DATABASE_URL'] or raise "DB_URL not present in environment"
+    ENV['DATABASE_URL'] or raise "DATABASE_URL not present in environment"
   end
 
   DB = Sequel.connect(connect_options)
