2ba4c5a9cd4a6de4ebe153e63e7ad9ccb2731033
Stephen Karger <stephen.karger@gmail.com>
2018-04-16 18:21:57 -0400

    Edits to make bin/sequel work

--- helpers/database_access.rb
+++ helpers/database_access.rb
@@ -7,7 +7,9 @@ module DatabaseAccess
     ENV['DATABASE_URL'] or raise "DATABASE_URL not present in environment"
   end
 
-  DB = Sequel.connect(connect_options)
+  unless const_defined?(:DB)
+    DB = Sequel.connect(connect_options)
+  end
 
   # DB connection must exist for plugin activation to work
   Sequel::Model.plugin :timestamps, update_on_create: true
