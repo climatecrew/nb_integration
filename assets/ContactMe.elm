b520f714f9a3def4a43b65513356d205770e62bc
Stephen Karger <stephen.karger@gmail.com>
2018-06-26 22:16:10 -0400

    Progress on contact form

--- assets/ContactMe.elm
+++ assets/ContactMe.elm
@@ -1,6 +1,8 @@
 module ContactMe exposing (Model, Msg, Flags, init, view, update)
 
-import Html exposing (Html, div)
+import Html exposing (Html, div, span, text, ul, li, input, label)
+import Html.Attributes exposing (id, class, placeholder, style, type_, for)
+import Html.Events exposing (onClick, onInput)
 
 
 type Msg
@@ -42,7 +44,57 @@ init flags =
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    mainView model
+
+
+mainView : Model -> Html Msg
+mainView model =
+    div
+        [ class "nb-integration-container" ]
+        [ div [ class "form" ]
+            [ ul [ class "flex-outer" ]
+                [ li [ class "section-start" ]
+                    [ label [ for "contact-name" ] [ text "First Name" ]
+                    , input [ id "contact-name", type_ "contact-name", placeholder "Required", onInput FirstName ] []
+                    , span
+                        [ class <| validationClass <| getViewError model "contact.first_name"
+                        , style [ ( "visibility", validationVisibility <| getViewError model "contact.name" ) ]
+                        ]
+                        [ text "First name must be present" ]
+                    ]
+                , li []
+                    [ label [ for "contact-email" ] [ text "Email" ]
+                    , input [ id "contact-email", type_ "contact-email", placeholder "Required", onInput Email ] []
+                    , span
+                        [ class <| validationClass <| getViewError model "contact.email"
+                        , style [ ( "visibility", validationVisibility <| getViewError model "contact.email" ) ]
+                        ]
+                        [ text "Email must be present" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+getViewError : Model -> String -> Bool
+getViewError model errorKey =
+    False
+
+
+validationVisibility : Bool -> String
+validationVisibility showErrors =
+    if showErrors then
+        "visible"
+    else
+        "hidden"
+
+
+validationClass : Bool -> String
+validationClass showErrors =
+    if showErrors then
+        "validation validation-errors"
+    else
+        "validation"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
