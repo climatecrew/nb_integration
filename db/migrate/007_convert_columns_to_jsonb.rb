ed526ada1c467097927ec51cbfa156c5c46b9270
Stephen Karger <stephen.karger@gmail.com>
2018-08-05 16:33:57 -0400

    Convert columns to jsonb

---
+++ db/migrate/007_convert_columns_to_jsonb.rb
@@ -0,0 +1,11 @@
+Sequel.migration do
+  up do
+    run "ALTER TABLE contact_requests ALTER COLUMN nb_person SET DATA TYPE jsonb USING nb_person::jsonb"
+    run "ALTER TABLE events ALTER COLUMN nb_event SET DATA TYPE jsonb USING nb_event::jsonb"
+  end
+
+  down do
+    run "ALTER TABLE contact_requests ALTER COLUMN nb_person SET DATA TYPE json USING nb_person::json"
+    run "ALTER TABLE events ALTER COLUMN nb_event SET DATA TYPE json USING nb_event::json"
+  end
+end
