b791124d9390a2e23070cba15e96af183bb199db
Stephen Karger <stephen.karger@gmail.com>
2018-08-14 08:14:07 -0400

    Enhance ErrorPresenter

--- helpers/error_presenter.rb
+++ helpers/error_presenter.rb
@@ -11,7 +11,18 @@ class ErrorPresenter
 
   def transform
     transformed = JSON.parse(body)
-    transformed['detail'] = transformed.delete('message')
-    transformed
+    if transformed.has_key?('validation_errors')
+      transformed['validation_errors'].map do |error|
+        {
+          'code' => transformed['code'],
+          'detail' => error
+        }
+      end
+    else
+      [{
+        'code' => transformed['code'],
+        'detail' => transformed['message']
+      }]
+    end
   end
 end
--- spec/helpers/error_presenter_spec.rb
+++ spec/helpers/error_presenter_spec.rb
@@ -9,10 +9,32 @@ RSpec.describe ErrorPresenter do
       }
     JSON
 
-    expected = {
+    expected = [{
       'code' => 'unauthorized',
       'detail' => 'You are not authorized to access this content. Your access token may be missing. The resource owner also may not have a permission level sufficient to grant access.'
-    }
+    }]
     expect(described_class.new(body: nb_body).transform).to eq(expected)
+  end
+
+  it "if NB validation_errors present it transforms them into error items with details" do
+    nb_body = <<~JSON
+    {
+      "code":"validation_failed",
+      "message":"Validation Failed.",
+      "validation_errors":[
+        "email is too short (minimum is 3 characters)",
+        "email 'c' should look like an email address"
+      ]
+    }
+    JSON
+
+    expected = [{
+      'code' => 'validation_failed',
+      'detail' => 'email is too short (minimum is 3 characters)'
+    }, {
+      'code' => 'validation_failed',
+      'detail' => "email 'c' should look like an email address"
+    }]
+    expect(described_class.new(body: nb_body).transform).to match_array(expected)
   end
 end
