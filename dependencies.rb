5f85df0bb311d49252156a0f9303e72424666eaa
Stephen Karger <stephen.karger@gmail.com>
2018-08-19 21:46:42 -0400

    Use glob to require helpers and models

--- dependencies.rb
+++ dependencies.rb
@@ -6,21 +6,8 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__), 'helpers/')
 require 'helpers/dotenv_loader'
 DotenvLoader.new.load
 
-require 'helpers/constants'
 require 'helpers/app_configuration'
-require 'helpers/database_access.rb'
-require 'helpers/path_provider'
-require 'helpers/client'
-require 'helpers/error_presenter'
-require 'helpers/nb_app_install'
-require 'helpers/request_oauth_access_token'
-require 'helpers/handle_oauth_callback'
-require 'helpers/handle_event_creation'
-require 'helpers/handle_contact_request_creation'
-require 'helpers/match_nb_person'
-require 'helpers/create_survey_response'
+Dir["helpers/*"].each { |file| require file }
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__), 'models/')
-require 'models/account'
-require 'models/event'
-require 'models/contact_request'
+Dir["models/*"].each { |file| require file }
